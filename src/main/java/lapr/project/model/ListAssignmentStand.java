4a7b5d38af0b68f4e09a9badf78a7fc44b2ae2bf
Miguel Barros <1151168@isep.ipp.pt>
2017-05-31 22:06:08 +0100

    ListAssignmentStand implementation

--- src/main/java/lapr/project/model/ListAssignmentStand.java
+++ src/main/java/lapr/project/model/ListAssignmentStand.java
@@ -12,13 +12,57 @@ import java.util.ArrayList;
  * @author Miguel Barros
  */
 public class ListAssignmentStand {
-    
-    
-        
-    }
-    
-    
-    
-    
-    
 
+    private ArrayList<AssignmentStand> listAssignmentStand;
+
+    /**
+     *
+     * Constructor
+     */
+    public ListAssignmentStand(ArrayList<AssignmentStand> lstAssignmentStand) {
+
+        this.listAssignmentStand = lstAssignmentStand;
+    }
+
+    /**
+     * 
+     */
+    public ListAssignmentStand() {
+
+        this.listAssignmentStand = new ArrayList<>(listAssignmentStand);
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public ArrayList<AssignmentStand> GetListAssignmentStand() {
+
+        return listAssignmentStand;
+    }
+
+    /**
+     * 
+     * @param listAssignmentStand 
+     */
+    public void setListAssignmentStand(ArrayList<AssignmentStand> listAssignmentStand) {
+        this.listAssignmentStand = listAssignmentStand;
+    }
+
+    /**
+     * 
+     * @param event
+     * @param std 
+     */
+    public void newAssignment(Event event, Stand std) {
+
+        AssignmentStand astd = new AssignmentStand(event, std);
+
+        if (astd.valida()) {
+            GetListAssignmentStand().add(astd);
+
+        }
+
+    }
+
+}
