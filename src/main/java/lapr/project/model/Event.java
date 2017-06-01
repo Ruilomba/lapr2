d90328b47415eb0620173149ac0ff5946205486e
RuiSL <1091237@isep.ipp.pt>
2017-06-01 22:23:37 +0100

    CreateEvent

--- src/main/java/lapr/project/model/Event.java
+++ src/main/java/lapr/project/model/Event.java
@@ -1,5 +1,29 @@
 package lapr.project.model;
 
-class Event {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Timer;
+import lapr.project.states.EventState;
+
+
+    public class Event implements Serializable {
+
+
+    private String titulo;
+    private String textoDescritivo;
+    private String local;
+    private Date dataInicio;
+    private Date dataFim;
+    private FAEList FaeList;
+    private OrganizerRegistration registoOrganizadores;
+    private ApplicationRegistration registoCandidaturas;
+    private AtribuitionList listaAtribuicoes;
+    private Date dataInicioSubCandidatura;
+    private Date dataFimSubCandidatura;
+    private EventType tipoEvento;
+    private EventState eventState;
+
     
 }
---
+++ src/main/java/lapr/project/states/EventState.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.states;
+
+/**
+ *
+ * @author RuiSL
+ */
+public interface EventState {
+    void setCriada();
+    boolean setFAEDefinidos();
+    boolean setDemoDefinidas();
+    boolean setCandidaturasAbertas();
+    boolean setCriarDemo();
+    boolean setCandidaturasExpoAbertas(); 
+}
