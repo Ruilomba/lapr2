d3eb4612e5c55245d7959f44dee845d741823583
RuiSL <1091237@isep.ipp.pt>
2017-06-02 17:40:11 +0100

    Method getStandRegistration FIX

--- src/main/java/lapr/project/model/EventCenter.java
+++ src/main/java/lapr/project/model/EventCenter.java
@@ -3,50 +3,46 @@ package lapr.project.model;
 import java.io.Serializable;
 
 public class EventCenter implements Serializable {
-   
+
     private static final long serialVersionUID = 1L;
     private EventRegistration eventRegistration;
     private UserRegistration userRegistration;
     private StandRegistration standRegistration;
 //    private AlgorithmRegistration algorithmRegistration;
 
-
     public EventCenter() {
         eventRegistration = new EventRegistration();
-        userRegistration= new UserRegistration();
+        userRegistration = new UserRegistration();
         //algorithmRegistration = new AlgorithmRegistration();
     }
 
     public EventRegistration getEventRegistration() {
         return eventRegistration;
     }
-    
-    public UserRegistration getUserRegistration(){
+
+    public UserRegistration getUserRegistration() {
         return userRegistration;
     }
-    
 
-    }
-
-     public StandRegistration getStandRegistration(){
+    public StandRegistration getStandRegistration() {
         return standRegistration;
     }
-    
-    /*
-    public AlgorithmRegistration getAlgorithmRegistration(){
-        return algorithmRegistration;
-    }
+}
 
-    public void setEventRegistration(EventRegistration eventRegistration) {
-        this.eventRegistration = eventRegistration;
-    }
+/*
+ public AlgorithmRegistration getAlgorithmRegistration(){
+ return algorithmRegistration;
+ }
 
-    public void setUserRegistration(UserRegistration userRegistration) {
-        this.userRegistration = userRegistration;
-    }
+ public void setEventRegistration(EventRegistration eventRegistration) {
+ this.eventRegistration = eventRegistration;
+ }
 
-    public void setAlgorithmRegistration(AlgorithmRegistration algorithmRegistration) {
-        this.algorithmRegistration = algorithmRegistration;
-    }
-    */
+ public void setUserRegistration(UserRegistration userRegistration) {
+ this.userRegistration = userRegistration;
+ }
 
+ public void setAlgorithmRegistration(AlgorithmRegistration algorithmRegistration) {
+ this.algorithmRegistration = algorithmRegistration;
+ }
+ */
